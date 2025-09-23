aee4697d16c84b5e466476702286d1e7a6fc5fae
Ishita Agarwal <ishita05agarwal@gmail.com>
2025-09-23 22:48:44 +0530

    updated code

--- Day81/MajorityElement.java
+++ Day81/MajorityElement.java
@@ -1,6 +1,34 @@
+import java.util.HashMap;
+import java.util.Scanner;
+
 public class MajorityElement {
+    static int findMajority(int[] arr){
+        HashMap<Integer,Integer> mp = new HashMap<>();
+        int max = 0;
+        int ans = 0;
+        for(int i = 0;i<arr.length;i++){
+            if(!mp.containsKey(arr[i])) mp.put(arr[i], 1);
+            else mp.put(arr[i], mp.get(arr[i])+1);
+        }
+        for(var num : mp.entrySet()){
+            if(num.getValue()>max){
+                max = num.getValue();
+                ans = num.getKey();
+            }
+        }
+        return ans;
+    }
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter no. of elements");
+        int n = sc.nextInt();
+        System.out.println("Enter elements");
+        int[] arr = new int[n];
+        for(int i = 0; i<n;i++){
+            arr[i] = sc.nextInt();
+        }
+        System.out.println(findMajority(arr));
+        sc.close();
     }
     
 }
