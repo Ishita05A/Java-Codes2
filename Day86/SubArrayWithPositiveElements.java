e9a8141ad41ab5a668968d189bca95f26bb9781a
Ishita Agarwal <ishita05agarwal@gmail.com>
2025-09-29 19:15:25 +0530

    updated code

--- Day86/SubArrayWithPositiveElements.java
+++ Day86/SubArrayWithPositiveElements.java
@@ -1,25 +1,22 @@
-package Day83;
+package Day86;
 
 import java.util.Scanner;
 
-public class LongestSubArrayWithKSum {
-    static int longestSubArray(int[] arr,int target){
+public class SubArrayWithPositiveElements {
+    static int longestSubArray(int[] arr, int target) {
         int max = 0;
-        int start = 0;
+        int st = 0;
         int sum = 0;
-        for(int end = 0;end<arr.length;end ++){
+        for(int end = 0;end<arr.length;end++){
             sum+=arr[end];
-            while(sum>target && start<=end){
-                sum-=arr[start];
-                start++;
+            while(sum > target && st<=end){
+                sum-=arr[st];
+                st++;
             }
-            if(sum == target) max = Math.max(max,end-start+1);
+            if(sum == target) max = Math.max(max, end-st+1);
         }
         return max;
-
     }
-
-    
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -33,7 +30,7 @@ public class LongestSubArrayWithKSum {
         System.out.println("Enter target");
         int x = sc.nextInt();
         System.out.println(longestSubArray(arr, x));
-        
         sc.close();
     }
+
 }
