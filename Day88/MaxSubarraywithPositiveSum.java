23f3c5da47d38104a0ad4790443d9e75ef66bd6a
Ishita Agarwal <ishita05agarwal@gmail.com>
2025-10-01 16:50:49 +0530

    updated code

--- Day88/MaxSubarraywithPositiveSum.java
+++ Day88/MaxSubarraywithPositiveSum.java
@@ -17,6 +17,7 @@ public class MaxSubarraywithPositiveSum {
         }
         return max;
     }
+    
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter no. of elements");
--- Day88/countSubArraywithTargetSum.java
+++ Day88/countSubArraywithTargetSum.java
@@ -1,32 +1,55 @@
 package Day88;
 
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class countSubArraywithTargetSum {
-    static int countSubArray(int[] arr,int target){
-        int count =0;
-        for(int i = 0; i<arr.length;i++){
+    static int countSubArray(int[] arr, int target) {
+        int count = 0;
+        for (int i = 0; i < arr.length; i++) {
             int sum = 0;
-            for(int j = i;j<arr.length;j++){
-                sum+=arr[j];
-                if(sum == target) count++;
+            for (int j = i; j < arr.length; j++) {
+                sum += arr[j];
+                if (sum == target)
+                    count++;
             }
         }
         return count;
     }
+
+    static int countSubArray2(int[] arr, int target) {
+        int prefixSum = 0;
+
+        int count = 0;
+        HashMap<Integer, Integer> mp = new HashMap<>();
+        mp.put(0, 1);
+        for (int i = 0; i < arr.length; i++) {
+            prefixSum += arr[i];
+             if (mp.containsKey(prefixSum - target))
+                count += mp.get(prefixSum - target);
+            if (!mp.containsKey(prefixSum))
+                mp.put(prefixSum, 1);
+
+            else
+                mp.put(prefixSum, mp.get(prefixSum) + 1);
+           
+        }
+        return count;
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter no. of elements");
         int n = sc.nextInt();
         System.out.println("Enter elements");
         int[] arr = new int[n];
-        for(int i = 0; i<n;i++){
+        for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
         }
         System.out.println("Enter Target");
         int x = sc.nextInt();
-        System.out.println(countSubArray(arr,x));
+        System.out.println(countSubArray(arr, x));
         sc.close();
     }
-    
+
 }
