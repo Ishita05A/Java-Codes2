0039832a5b56393d834fd0780473b92a640b6b61
Ishita Agarwal <ishita05agarwal@gmail.com>
2025-10-01 14:41:07 +0530

    updated code

---
+++ Day88/countSubArraywithTargetSum.java
@@ -0,0 +1,32 @@
+package Day88;
+
+import java.util.Scanner;
+
+public class countSubArraywithTargetSum {
+    static int countSubArray(int[] arr,int target){
+        int count =0;
+        for(int i = 0; i<arr.length;i++){
+            int sum = 0;
+            for(int j = i;j<arr.length;j++){
+                sum+=arr[j];
+                if(sum == target) count++;
+            }
+        }
+        return count;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter no. of elements");
+        int n = sc.nextInt();
+        System.out.println("Enter elements");
+        int[] arr = new int[n];
+        for(int i = 0; i<n;i++){
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("Enter Target");
+        int x = sc.nextInt();
+        System.out.println(countSubArray(arr,x));
+        sc.close();
+    }
+    
+}
