db38a7df0a6531e2452a2771fcdca3b4f036196b
Ishita Agarwal <ishita05agarwal@gmail.com>
2025-10-02 14:17:34 +0530

    updated code

---
+++ Day89/postfixToPrefix.java
@@ -0,0 +1,32 @@
+package Day89;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+// Input: expression = "abc*+d-"
+// Output: "-+a*bcd"
+
+public class postfixToPrefix {
+    static String convert(String str){
+        Stack<String> st = new Stack<>();
+        for(int i = 0;i<str.length();i++){
+            char ch = str.charAt(i);
+            int ascii = (int)ch;
+            if(ascii >= 48 && ascii <=57) st.push(ch+"");
+            else{
+                String v2 = st.pop();
+                String v1 = st.pop();
+                st.push(ch+v1+v2);
+            }
+        }
+        return st.peek();
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Expression");
+        String str = sc.nextLine();
+        System.out.println(convert(str));
+        sc.close();
+    }
+    
+}
