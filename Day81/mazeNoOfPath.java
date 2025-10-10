b6883708117d1af00fb0980cb85649b37ae3653f
Ishita Agarwal <ishita05agarwal@gmail.com>
2025-10-10 21:19:49 +0530

    updated code

--- Day81/mazeNoOfPath.java
+++ Day81/mazeNoOfPath.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class mazeNoOfPath {
     static int maze(int sr,int sc,int er,int ec){
         if(sr>er || sc > ec) return 0;
+        
         if(sr == er && er == sc) return 1; 
         int rightWays = maze( sr+1, sc, er, ec);
         int downWays = maze( sr, sc+1, er, ec);
