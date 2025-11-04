2e46f4fd4d18741f54aa26198330ea8360233d7c
Ishita Agarwal <ishita05agarwal@gmail.com>
2025-11-04 18:24:06 +0530

    updated code

---
+++ Day105/Factors.java
@@ -0,0 +1,19 @@
+package Day105;
+
+import java.util.Scanner;
+
+public class Factors {
+    public static void main(String[] args) {
+         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number");
+        int n = sc.nextInt();
+        for(int i = 1;i*i<=n;i++){
+            if(n % i==0){
+                System.out.println(i);
+                if(i != n/i) System.out.println(n/i);
+            } 
+        }
+        sc.close();
+    }
+    
+}
