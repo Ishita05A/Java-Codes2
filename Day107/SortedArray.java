c4893fd0980ce051d75421847d9a12567d5afad5
Ishita Agarwal <ishita05agarwal@gmail.com>
2025-11-06 14:09:22 +0530

    updated code

--- Day107/SortedArray.java
+++ Day107/SortedArray.java
@@ -1,8 +1,30 @@
 package Day107;
 
+import java.util.Scanner;
+
 public class SortedArray {
-    public static void main(String[] args) {
-        
+    static boolean is_sorted(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < n-i-1; j++) {
+                if(arr[j] > arr[j+1]) return false;
+            }
+        }
+        return true;
+
     }
-    
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter no. of elements");
+        int n = sc.nextInt();
+        System.out.println("Enter elements");
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.println(is_sorted(arr));
+        sc.close();
+    }
+
 }
