50ab64acb3562a118b571c709328bde0050a6306
Ishita Agarwal <ishita05agarwal@gmail.com>
2025-11-25 03:35:55 +0530

    updated code

---
+++ Day123/FindMinimumDivisor.java
@@ -0,0 +1,47 @@
+package Day123;
+
+import java.util.Scanner;
+
+public class FindMinimumDivisor {
+     static int findMax(int[] arr) {
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < arr.length; i++)
+            max = Math.max(max, arr[i]);
+        return max;
+    }
+    static int sum_of_division(int[] arr,int n){
+        int sum = 0;
+        for(int num : arr) sum+=((int)Math.ceil((double)num/n));
+        return sum;
+    }
+    static int miminum_divisor(int[] arr,int limit) {
+        int st = 1;
+        int end = findMax(arr);
+        int ans = -1;
+        while (st<=end) {
+            int mid = (end-st)/2+st;
+            int sum = sum_of_division(arr, mid);
+            if(sum<=limit){
+                ans = mid;
+                end = mid-1;
+            }
+            else st = mid + 1;
+        }
+        return ans;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter no. of elements");
+        int n = sc.nextInt();
+        System.out.println("Enter elements");
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("Enter limit");
+        int m = sc.nextInt();
+        System.out.println(miminum_divisor(arr, m));
+        sc.close();
+    }
+    
+}
