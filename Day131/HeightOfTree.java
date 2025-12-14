c2c2b4d8e54f43e2a839aa373a3400a84e3c29af
Ishita Agarwal <ishita05agarwal@gmail.com>
2025-12-15 01:35:03 +0530

    updated code

--- Day131/HeightOfTree.java
+++ Day131/HeightOfTree.java
@@ -13,6 +13,7 @@ public class HeightOfTree {
         if(root == null) return 0;
         return 1+Math.max(height(root.left), height(root.right));
     }
+    
     static int edgeheight(Node root){
         if(root == null || (root.left == null && root.right == null)) return 0;
         return 1+Math.max(edgeheight(root.left), edgeheight(root.right));
@@ -29,6 +30,10 @@ public class HeightOfTree {
         return isBalalanced(root.left) && isBalalanced(root.right);
         
     }
+    static int findSum(Node root){
+        if(root == null) return 0;
+        return root.val + Math.max(findSum(root.left), findSum(root.right));
+    }
     static int diameter(Node root){
         if(root == null ||(root.left == null && root.right == null)) return 0;
         int leftAns = diameter(root.left);
@@ -38,6 +43,20 @@ public class HeightOfTree {
         if(root.right != null) mid++;
         return Math.max(Math.max(rightAns, leftAns), mid);
         
+    }
+    static int findMaxSum(Node root, int max[]){
+        if(root == null) return 0;
+        int left = Math.max(0,findMaxSum(root.left, max));
+        int right = Math.max(0, findMaxSum(root.right, max));
+        max[0] = Math.max(left+right+root.val , max[0]);
+        return Math.max(left,right)+root.val;
+    } 
+    static int maxPathSum(Node root){
+        int max[] = new int[1];
+        max[0] = Integer.MIN_VALUE;
+        findMaxSum(root, max);
+        return max[0];
+
     }
     public static void main(String[] args) {
          Node root = new Node(1);
@@ -62,6 +81,7 @@ public class HeightOfTree {
         System.out.println(height(root));
         System.out.println(isBalalanced(root));
         System.out.println(diameter(root));
+        System.out.println(maxPathSum(root));
     }
     
 }
