d86562c33c73a6f60ec4d4f1700f3a90ca024f8a
Ishita Agarwal <ishita05agarwal@gmail.com>
2025-12-18 01:35:16 +0530

    updated code

---
+++ Day134/MorrisInOrderTraversal.java
@@ -0,0 +1,59 @@
+package Day134;
+import java.util.*;
+public class MorrisInOrderTraversal {
+    public static class Node{
+        int val;
+        Node left;
+        Node right;
+        Node(int val){
+            this.val = val;
+        }
+    }
+    static List<Integer> morris_inorder_traversal(Node root){
+        List<Integer> ans = new ArrayList<>();
+        Node curr = root;
+        while(curr != null){
+            if(curr.left == null){
+                ans.add(curr.val);
+                curr = curr.right;
+            }
+            else{
+                Node prev = curr.left;
+                while(prev.right != null && prev.right != curr){
+                    prev = prev.right;
+                }
+                if(prev.right == null){
+                    prev.right = curr;
+                    curr = curr.left;
+                }
+                else{
+                    ans.add(curr.val);
+                    curr = curr.right;
+                }
+            }
+        }
+        return ans;
+    }
+    public static void main(String[] args) {
+        Node root = new Node(1);
+        Node a = new Node(2);
+        Node b = new Node(3);
+        Node c = new Node(4);
+        Node d = new Node(5);
+        Node e = new Node(6);
+        Node f = new Node(7);
+        Node g = new Node(8);
+        Node h = new Node(9);
+        Node i = new Node(10);
+        root.left = a;
+        root.right = b;
+        a.left = c;
+        a.right = d;
+        d.left = g;
+        b.left = e;
+        b.right = f;
+        f.left = h;
+        f.right = i;
+        System.out.println(morris_inorder_traversal(root));
+    }
+}
