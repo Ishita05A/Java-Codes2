48c51f6697fb352d6200c2e55eff34bf25d4e545
Ishita Agarwal <ishita05agarwal@gmail.com>
2025-12-25 01:58:10 +0530

    updated code

---
+++ Day141/removeDuplicate.java
@@ -0,0 +1,27 @@
+package Day141;
+
+import java.util.Scanner;
+
+public class removeDuplicate {
+    static int remove_duplicate(int[] arr){
+        int k = 1;
+        for(int i = 1; i<arr.length;i++){
+            if(arr[i] != arr[i-1]) arr[k++] = arr[i];
+        }
+        return k;
+
+    }
+    public static void main(String[] args) {
+         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter no. of elements");
+        int n = sc.nextInt();
+        System.out.println("Enter elements");
+        int[] arr = new int[n];
+        for(int i = 0; i<n;i++){
+            arr[i] = sc.nextInt();
+        }
+        System.out.println(remove_duplicate(arr));
+        sc.close();
+    }
+    
+}
