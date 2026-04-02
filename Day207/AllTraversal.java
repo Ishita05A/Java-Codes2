ff94776645bbcf597483cdd64e56a4a460ef279c
Ishita Agarwal <ishita05agarwal@gmail.com>
2026-04-02 22:40:11 +0530

    updated code

---
+++ Day207/AllTraversal.java
@@ -0,0 +1,95 @@
+package Day207;
+
+import java.util.Stack;
+
+public class AllTraversal {
+    static class Node{
+        int val;
+        Node left;
+        Node right;
+        Node(int val){
+            this.val = val;
+        }
+    }
+    static void levelOrder(Node root){
+        Stack<Node> st = new Stack<>();
+        st.push(root);
+        while(!st.isEmpty()){
+            int size = st.size();
+            for(int i = 0;i<size;i++){
+                Node top = st.pop();
+                System.out.print(top.val+" ");
+                if(top.right != null) st.push(top.right); 
+                if(top.left != null) st.push(top.left);
+            }
+            System.out.println();
+        }
+    }
+    static void inOrder_iterative(Node root){
+        Node node = root;
+        Stack<Node> st = new Stack<>();
+        while(!st.empty() || node != null){
+            if(node != null){
+                st.push(node);
+                node = node.left;
+            }
+            else{
+                Node top = st.pop();
+                System.out.print(top.val+" ");
+                node = top.right;
+            }
+        }
+    }
+    static void inOrder(Node root){
+        if(root.left != null) inOrder(root.left);
+        System.out.print(root.val+" ");
+        if(root.right != null) inOrder(root.right);
+    }
+    static void preOrder_Iterative(Node root){
+        Stack<Node> st = new Stack<>();
+        st.push(root);
+        while(!st.isEmpty()){
+            Node top = st.pop();
+            System.out.print(top.val+" ");
+            if(top.right != null) st.push(top.right);
+            if(top.left != null) st.push(top.left);
+        }
+    }
+    static void preOrder(Node root){
+        System.out.print(root.val+" ");
+        if(root.left != null) preOrder(root.left);
+        if(root.right != null) preOrder(root.right);
+    }
+    static void postorder(Node root){
+        if(root.left != null) postorder(root.left);
+        if(root.right != null) postorder(root.right);
+        System.out.print(root.val+" ");
+    }
+    public static void main(String[] args) {
+        Node root = new Node(1);
+        Node a = new Node(2);
+        Node b = new Node(3);
+        Node c = new Node(4);
+        Node d = new Node(5);
+        Node e = new Node(6);
+        Node f = new Node(7);
+        root.left = a;
+        root.right = b;
+        a.left = c;
+        a.right = d;
+        b.left = e;
+        b.right = f;
+        inOrder(root);
+        System.out.println();
+        preOrder(root);
+        System.out.println();
+        postorder(root);
+        System.out.println();
+        levelOrder(root);
+        inOrder_iterative(root);
+        System.out.println();
+        preOrder_Iterative(root);
+
+    }
+    
+}
